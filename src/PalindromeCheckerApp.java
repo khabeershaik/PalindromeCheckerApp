9c374a80ef2ddc388fbc9b186dde1bf91048e8bb
khabeershaik <khabeershaik06@gamil.com>
2026-02-25 09:59:52 +0530

    UC:3 Palindrome using string reverse logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,19 +3,18 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker");
-        System.out.println("Version 2.0");
+        System.out.println("Version 3.0");
 
         String input = "madam";   // Hardcoded string
-        boolean isPalindrome = true;
+        String reversed = "";
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Reverse the string
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-        if (isPalindrome) {
+        // Compare original and reversed
+        if (input.equals(reversed)) {
             System.out.println(input + " is a Palindrome");
         } else {
             System.out.println(input + " is NOT a Palindrome");

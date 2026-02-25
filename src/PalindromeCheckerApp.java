d2be582f6793b6b502849c8921d12b4ef6832d56
khabeershaik <khabeershaik06@gamil.com>
2026-02-25 09:36:03 +0530

    UC1: Welcome to Palimdrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker");
+        System.out.println("Welcome to Palindrome Checker Checker");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized Successfully");
     }
 }

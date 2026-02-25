28a1225fd5d5e9d4cd0cdcfdec97397551e00b8b
khabeershaik <khabeershaik06@gamil.com>
2026-02-25 09:52:27 +0530

    UC2: Hardcored palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,26 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Checker");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized Successfully");
+
+        System.out.println("Welcome to Palindrome Checker");
+        System.out.println("Version 2.0");
+
+        String input = "madam";   // Hardcoded string
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+
+        System.out.println("Program Finished");
     }
 }
